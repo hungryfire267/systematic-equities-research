398ae4eb598274952155dc0e460f6dd34eff6002
hungryfire267 <gordon123454321@gmail.com>
2026-06-05 00:45:19 +1000

    Added cpi data

--- scripts/get_macro.py
+++ scripts/get_macro.py
@@ -12,12 +12,17 @@ import yfinance as yf
 
 from pydantic import BaseModel, Field
 
-class ReleaseEntry(BaseModel): 
-    reference_month: dt.date = Field(description="The first day of the reference month, e.g., '2026-04-01'")
-    release_date: dt.date = Field(description="The date of the release, e.g., '2026-04-15'")
+class ReleaseEntry(BaseModel):
+    month_of_release: dt.date = Field(
+        description="The calendar month when the data was released, e.g., 'May 2026'"
+    )
+    release_date: dt.date = Field(
+        description="The precise release date formatted as YYYY-MM-DD"
+    )
+
 
 class ABSReleaseCalendar(BaseModel):
-    releases: list[ReleaseEntry] = Field(description="Chronological list of all release dates")
+    releases: list[ReleaseEntry] = Field(description="Chronological list of all release entries")
     
  
 
@@ -38,6 +43,106 @@ class MacroPipeline(ABC):
         raw_data = self.get_raw_data() 
         clean_data = self.clean_data(raw_data)
         return clean_data
+
+class CPI(MacroPipeline): 
+    def __init__(self, start_date, end_date): 
+        super().__init__(start_date, end_date)
+        
+        self.client = genai.Client()
+        self.series_mapping = {
+            "monthly": "A130393721F", 
+            "quarterly": "A2325847F"
+        }
+        
+        
+    def get_cpi_dates(self, cpi_type: str): 
+        if cpi_type not in ("monthly", "quarterly"): 
+            raise ValueError("cpi_type must be either 'monthly' or 'quarterly'")
+        
+        threshold_date = dt.date(2025, 10, 1)
+        if self.start_date >= threshold_date: 
+            release_dates_df = self.get_abs_release_dates(cpi_type="monthly")
+        elif self.start_date <= dt.date(2025, 10, 1) and self.end_date >= dt.date(2025, 10, 1):
+            release_dates_df_quarter = self.get_abs_release_dates(cpi_type = "quarterly")
+            release_dates_df_month = self.get_abs_release_dates(cpi_type = "monthly")
+        else: 
+            release_dates_df = self.get_abs_release_dates(cpi_type="quarterly")
+        return release_dates_df
+        
+    def get_abs_release_dates(self, cpi_type: str) -> pd.DataFrame:
+        start_date_month = self.start_date.strftime("%B")
+        start_date_year = self.start_date.year
+        
+        try: 
+            prompt_instruction = f"""
+                Generate a clean historical release schedule for the Australian Bureau of Statistics (ABS) 
+                Labour Force CPI rate from {start_date_month} {start_date_year} to {end_date_month} {end_date_year}. 
+
+                For each entry, provide the month in which the release actually occurred and its exact release date. 
+                Keep the entries in strict chronological order.
+            """
+            
+            response = self.ai_client.models.generate_content(
+                model="gemini-3.1-flash-lite",
+                contents = prompt_instruction, 
+                config = types.GenerateContentConfig(
+                    response_mime_type = "application/json", 
+                    response_schema = ABSReleaseCalendar, 
+                    temperature = 0.05
+                )
+            )
+        
+            response_json = json.loads(response.text)
+            release_date_df = pd.DataFrame(response_json["releases"])
+            return release_date_df
+        
+        except Exception as e:
+            print()
+        
+        
+        pass
+        
+        
+    def load_cpi_data(self, url) -> pd.DataFrame: 
+        cpi_df = pd.read_excel(url, sheet_name="Data1", header=0)
+        cpi_df = cpi_df.set_index("Unnamed: 0")
+        cpi_df.index.name = "Date"
+        return cpi_df 
+    
+    def get_raw_data(self) -> pd.DataFrame: 
+        url_type_dict = { 
+            "monthly": r"https://www.abs.gov.au/statistics/economy/price-indexes-and-inflation/consumer-price-index-australia/apr-2026/640101.xlsx",
+            "quarterly": r"https://www.abs.gov.au/statistics/economy/price-indexes-and-inflation/consumer-price-index-australia/sep-quarter-2025/640101.xlsx"
+        }
+        
+        df_monthly = self.load_cpi_data(url_type_dict["monthly"])
+        df_quarterly = self.load_cpi_data(url_type_dict["quarterly"])
+        
+        return [df_monthly, df_quarterly]
+    
+    def wrangle_data(self, df: pd.DataFrame, df_type: str) -> pd.DataFrame:
+        series_id = self.series_mapping[df_type]
+        print(series_id)
+        df = df[df.columns[df.loc["Series ID"].eq(series_id)]]
+        df.columns = ["CPI"]
+        series_id_index = df.index.get_loc("Series ID")
+        df = df.iloc[series_id_index + 1: ]
+        df = df.reset_index()
+        df["Date"] = pd.to_datetime(df["Date"]).dt.date
+        if df_type == "monthly": 
+            df = df[df["Date"] >= dt.date(2025, 11, 1)]
+        return df
+        
+    
+    def clean_data(self, raw_data: pd.DataFrame) -> pd.DataFrame: 
+        df_monthly, df_quarterly = raw_data.copy()
+        
+        df_monthly = self.wrangle_data(df_monthly, "monthly")
+        df_quarterly = self.wrangle_data(df_quarterly, "quarterly")
+
+        df = pd.concat([df_quarterly, df_monthly], axis=0).reset_index(drop=True)
+        return df
+        
     
 class CurrencyRates(MacroPipeline): 
     def __init__(self, start_date, end_date):
@@ -105,9 +210,11 @@ class UnemploymentRate(MacroPipeline):
         
         try: 
             prompt_instruction = f"""
-                Generate a clean historical release schedule for the Australia Bureau of Statistics (ABS) 
-                Labour Force unemployment rate from {start_date_month} {start_date_year} to {end_date_month} {end_date_year}. Keep the entries in 
-                chronological order.
+                Generate a clean historical release schedule for the Australian Bureau of Statistics (ABS) 
+                Labour Force unemployment rate from {start_date_month} {start_date_year} to {end_date_month} {end_date_year}. 
+
+                For each entry, provide the month in which the release actually occurred and its exact release date. 
+                Keep the entries in strict chronological order.
             """
             response = self.ai_client.models.generate_content(
                 model="gemini-3.1-flash-lite",
@@ -153,9 +260,11 @@ class UnemploymentRate(MacroPipeline):
         end_condition = df["Date"] <= self.end_date
         df = df[start_condition & end_condition].reset_index(drop=True)
         
-        
-        
         release_dates_df = self.get_unemployment_dates() 
+        print(release_dates_df)
+        if release_dates_df is not None: 
+            df = df.merge(release_dates_df, how="left", left_on="Date", right_on="reference_month")
+            df = df.drop(columns=["reference_month"])
         return df
     
 
@@ -203,6 +312,14 @@ class interest_rates:
         ocr_final = self.get_interest_rates()
         ocr_final_df = self.clean_interest_rates(ocr_final)
         return ocr_final_df
+    
+class YieldCurves: 
+    def __init__(self): 
+        super().__init__(start_date, end_date)
+        
+            
+        
+        
 
 def get_day_prefix(day): 
     if day >= 11 and day <= 13:
@@ -212,15 +329,14 @@ def get_day_prefix(day):
 
 if __name__ == "__main__": 
     end_date = dt.datetime.today().date()
-    start_date = end_date - dt.timedelta(days=1461)
-    
-    end_date_year = end_date.year
+    start_date = end_date - dt.timedelta(days=200)
+    print(start_date)
+    print(end_date)
     load_dotenv()
     
-    currency_data = CurrencyRates(start_date, end_date).run_data()
-    print(currency_data.head())
-    unemployment_data = UnemploymentRate(start_date, end_date).run_data()
-    print(unemployment_data.head())
+    
+    raw_data = CPI(start_date, end_date).run_data()
+    
     
 
     
