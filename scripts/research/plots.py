8ed964789b8a167127870c6d05507ebf36233570
hungryfire267 <gordon123454321@gmail.com>
2026-06-14 18:29:39 +1000

    Added

--- scripts/research/plots.py
+++ scripts/research/plots.py
@@ -6,6 +6,8 @@ import seaborn as sns
 
 category_cols = { 
     "beta": ["market_beta", "industry_beta", "market_resid_vol", "industry_resid_vol"], 
+    "mean_volatility": ["mean_volatility"],
+    "microstructure": ["dv_liquidity", "amihud"],
     "momentum": ["momentum", "id"],
     "reversal": ["reversal", "rsr"]
 }
@@ -46,12 +48,15 @@ def plot_ic_timeseries(ic_df_dict: dict, category_name: str):
     category_dicts = get_final_dict(ic_df_dict, category_name)
     n = len(category_dicts.keys())
     
-    if (n == 2): 
+    if (n == 1):
+        fig, axs = plt.subplots(1, 1, figsize=(8, 6))
+    elif (n == 2): 
         fig, axs = plt.subplots(1, 2, figsize=(16, 6)) 
     elif (n == 4): 
         fig, axs = plt.subplots(2, 2, figsize=(16, 12))       
     
-    axs = axs.ravel() 
+    axs = np.atleast_1d(axs).ravel()
+    fig.suptitle(f"Time Series of IC score across {category_name} signals")
     
     i = 0 
     for category, category_signal_names in category_dicts.items(): 
@@ -59,8 +64,43 @@ def plot_ic_timeseries(ic_df_dict: dict, category_name: str):
             df = ic_df_dict[signal_name].copy()
             df["IC_63D"] = df["IC"].rolling(63).mean()
             axs[i].plot(df["Date"], df["IC_63D"], linewidth=2, label=f"{signal_name}_63D")
+        
         axs[i].legend()
+        axs[i].set_title(f"Line plot of {category}", fontsize=13)
+        axs[i].set_xlabel(f"Date", fontsize=12)
+        axs[i].set_ylabel("IC Score", fontsize=12)
         i += 1
             
     plt.show()
+
+def plot_ic_summary_bar(ic_df_dict: pd.DataFrame, summary_df: pd.DataFrame, category_name: str, metric: str): 
+    category_dicts = get_final_dict(ic_df_dict, category_name)
+    n = len(category_dicts.keys())
     
+    if (n == 1):
+        fig, axs = plt.subplots(1, 1, figsize=(8, 6))
+    elif (n == 2): 
+        fig, axs = plt.subplots(1, 2, figsize=(16, 6)) 
+    elif (n == 4): 
+        fig, axs = plt.subplots(2, 2, figsize=(16, 12))       
+    
+    
+    fig.suptitle(f"Summary statistics of {metric} across {category_name} signals", fontsize=14)
+    
+    axs = np.atleast_1d(axs).ravel()
+    
+    i = 0
+    for category, category_signal_names in category_dicts.items(): 
+        df = summary_df[category_signal_names].copy() 
+        
+        values = df.loc[metric].sort_values()
+        
+        axs[i].set_title(f"Bar plot of {category}", fontsize=13)
+        axs[i].set_xlabel(f"Signal name", fontsize=12)
+        axs[i].set_ylabel(f"{metric}", fontsize=12)
+        axs[i].barh(values.index, values.values)
+        axs[i].axvline(0, linestyle="--")
+        
+        i += 1
+    plt.tight_layout()
+    plt.show()
---
+++ scripts/research/quintile.py
@@ -0,0 +1,28 @@
+import numpy as np
+import os
+import pandas as pd
+
+
+class Quintile: 
+    def __init__(self,  return_df: pd.DataFrame, signal_df: pd.DataFrame):
+        self.return_df = return_df
+        self.signal_df = signal_df
+        
+        self.return_df["Date"] = pd.to_datetime(self.return_df["Date"])
+        self.signal_df["Date"] = pd.to_datetime(self.signal_df["Date"])
+        
+        self.factor_list = list(self.signal_df.columns[2:])
+        
+    def merge_dfs(self): 
+        self.df = pd.merge(left = self.return_df, right = self.signal_df, how = "outer", on = ["Date", "Ticker"])
+        
+        print(self.df)
+        
+    def calculate(self, factor_col: str, target_col: str): 
+        pass
+        
+    def run_data(self): 
+        self.merge_dfs()
+        
+        print(self.factor_list)
+            
