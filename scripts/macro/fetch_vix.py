a75a7076edfdd88022410dbdf2ee15ccee3bc8de
hungryfire267 <gordon123454321@gmail.com>
2026-06-27 15:30:24 +1000

    Added some macro data in

---
+++ scripts/macro/fetch_vix.py
@@ -0,0 +1,37 @@
+import datetime as dt
+import numpy as np
+import pandas as pd 
+from pathlib import Path
+import yfinance as yf 
+
+
+
+
+class VIX: 
+    def __init__(self, start_date, end_date): 
+        self.start_date = start_date
+        self.end_date = end_date
+        
+    def fetch_vix(self): 
+        vix_data = yf.download(
+            tickers="^AXVI", auto_adjust=True, start=self.start_date, end=self.end_date
+        )
+        
+        final_df = vix_data["Close"].reset_index()
+        final_df.index.name = None
+        
+        return final_df
+    
+    def run_data(self): 
+        final_df = self.fetch_vix()
+        final_df.to_parquet()
+        
+        
+        
+                
+if __name__ == "__main__":
+    end_date = pd.to_datetime("2026-06-13")
+    start_date = end_date - dt.timedelta(days=1461)
+    
+     
+    VIX(start_date=start_date, end_date=end_date).fetch_vix()
